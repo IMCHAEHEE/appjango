08ce33f76c57bf01f7b210077216d74a0e237c5d
jenny <12venus08@naver.com>
2020-10-28 15:08:06 +0900

    선호

--- app/src/main/java/com/sungkyul/appjango/MainActivity.java
+++ app/src/main/java/com/sungkyul/appjango/MainActivity.java
@@ -5,8 +5,13 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.ListView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
@@ -27,4 +32,5 @@ public class MainActivity extends AppCompatActivity {
             }
         });
     }
+
 }
